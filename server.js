92fbbb4b79ef95071a6d0a414f18bc28558734cc
bramos013 <brunoramos013@gmail.com>
2020-11-08 19:42:53 -0300

    Reestruturando arquivos

--- server.js
+++ server.js
@@ -9,23 +9,7 @@ mongoose.connect("mongodb://localhost:27017/nodeapi",
  {useNewUrlParser:true, useUnifiedTopology:true});
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-
-
-/*/Primeira rota 
-app.get('/',(req, res) =>{ 
-    res.send('Hello NodeJS');
-});*/
-
-//Inserindo um Produto
-app.get('/',(req, res) =>{ 
-    Product.create({
-        title: "React Native",
-        description: "Build native apps with React",
-        url: "http://github.com/facebook/react-native"    
-});
-    return res.send("Product Created");
-});
+//Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001); //Aplicação rodará na porta 3001
